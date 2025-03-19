2dad3d06ae897d846c1065e7bd731765c8d2b289
Khalil Sahim <la-tua-email@esempio.com>
2025-03-19 21:22:47 +0100

    aGGIORNAMENTO CONFIG.JS 2

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCE_MXk6bFR2Z36rhbjjukS_6zvNC3SOaw",
   authDomain: "ediplan-ore-lavoro.firebaseapp.com",
   projectId: "ediplan-ore-lavoro",
-  storageBucket: "ediplan-ore-lavoro.firebasestorage.app",
+  storageBucket: "ediplan-ore-lavoro.appspot.com", // Corretto rispetto a firebasestorage.app
   messagingSenderId: "700515290610",
   appId: "1:700515290610:web:6d6da414c787597842af6b",
   measurementId: "G-NGY003GTJV"
@@ -12,3 +12,9 @@ const firebaseConfig = {
 // Inizializza Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+// Configurazione aggiuntiva per migliorare la connettività
+firebase.firestore().settings({
+  cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED,
+  experimentalForceLongPolling: true
+});
