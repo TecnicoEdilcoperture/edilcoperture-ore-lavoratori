ae4bb9f25e893ae6e44917e31fa28dcbe0167b49
Khalil Sahim <la-tua-email@esempio.com>
2025-03-19 20:49:03 +0100

    Aggiornamento 4

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "inserisci-la-tua-api-key-reale",
-    authDomain: "il-tuo-project-id.firebaseapp.com",
-    projectId: "il-tuo-project-id",
-    storageBucket: "il-tuo-project-id.appspot.com",
-    messagingSenderId: "il-tuo-sender-id",
-    appId: "il-tuo-app-id"
+  apiKey: "AIzaSyCDmn4i_rPR01HFJdwZpKLKJsOX60iR4NM",
+  authDomain: "edilcoperture-ore-lavoratori.firebaseapp.com",
+  projectId: "edilcoperture-ore-lavoratori",
+  storageBucket: "edilcoperture-ore-lavoratori.appspot.com",
+  messagingSenderId: "106929196358",
+  appId: "1:106929196358:web:b57d2f21e908b0a0f5a94c"
 };
-  
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
